a43e523c83d18321f04b0b6ea948b1e9db3a901f
Ilya Edelshtein <ilya.edelstein@konrnit.com>
2022-07-25 14:36:05 +0300

    added solution for product and models task

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -1,10 +1,27 @@
-const mongoose = require('mongoose');
-const connection = require('../libs/connection');
+const mongoose = require("mongoose");
+const connection = require("../libs/connection");
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  }
 });
 
 const categorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  subcategories: [
+    subCategorySchema
+    // {
+    //   title: {
+    //     type: String,
+    //     required: true
+    //   }
+    // }
+  ]
 });
-
-module.exports = connection.model('Category', categorySchema);
+// const Subcategory = mongoose.model("Subcategory", subCategorySchema);
+module.exports = connection.model("Category", categorySchema);
--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,31 @@
-const mongoose = require('mongoose');
-const connection = require('../libs/connection');
+const mongoose = require("mongoose");
+const connection = require("../libs/connection");
+const Category = require("../models/Category");
 
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category"
+  },
+  subcategory: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category"
+  },
+  images: [String]
 });
 
-module.exports = connection.model('Product', productSchema);
+module.exports = connection.model("Product", productSchema);
